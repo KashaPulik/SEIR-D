0dffc9476fa2a0883aea8690d20734770b29b773
Kulik Pavel <pashakulik58@gmail.com>
2024-04-13 22:17:29 +0700

    5 try doesn't work, but I like the code now

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
-double N(double S, double E, double I, double R, double D)
+double alphaE = 0.999;
+double alphaI = 0.999;
+double k = 0.042;
+double ro = 0.952;
+double beta = 0.999;
+double mu = 0.0188;
+double c = 1;
+
+double dS(double N, double S, double E, double I)
+{
+    return (-c * ((alphaI * S * I) / N) + ((alphaE * S * E) / N));
+}
+
+double dE(double N, double S, double E, double I)
+{
+    return (c * ((alphaI * S * I) / N) + ((alphaE * S * E) / N) - ((k + ro) * E));
+}
+
+double dI(double E, double I)
+{
+    return (k * E - beta * I - mu * I);
+}
+
+double dR(double E, double I)
+{
+    return (beta * I + ro * E);
+}
+
+double dD(double I)
+{
+    return (mu * I);
+}
+
+double count_N(double S, double E, double I, double R, double D)
 {
     return S + E + I + R + D;
 }
@@ -8,14 +41,19 @@ double N(double S, double E, double I, double R, double D)
 double SEIRD(double S0, double E0, double I0, double R0, double D0, int t0, double t, double h)
 {
     double S = S0, E = E0, I = I0, R = R0, D = D0;
+    double Sn, En, In;
     int i = t0;
-    t /= h;
-    for (; i < t; i++) {
-        S = S0 + h * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0));
-        E = E0 + h * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0) - 0.994 * E0);
-        I = I0 + h * (0.042 * E0 - 1.0178 * I0);
-        R = R0 + h * (0.999 * I0 + 0.952 * E0);
-        D = D0 + h * (0.0188 * I0);
+    int N;
+    for (; i < t - 1; i++) {
+        N = count_N(S, E, I, R, D);
+        Sn = S0 + h * dS(N, S0, E0, I0);
+        En = E0 + h * dE(N, S0, E0, I0);
+        In = I0 + h * dI(E0, I0);
+        S = S0 + h * 0.5 * (dS(N, S0, E0, I0) + dS(N, Sn, En, In));
+        E = E0 + h * 0.5 * (dE(N, S0, E0, I0) + dE(N, Sn, En, In));
+        I = I0 + h * 0.5 * (dI(E0, I0) + dI(En, In));
+        R = R0 + h * 0.5 * (dR(E0, I0) + dR(En, In));
+        D = D0 + h * 0.5 * (dD(I0) + dD(In));
         S0 = S;
         E0 = E;
         I0 = I;
@@ -27,9 +65,10 @@ double SEIRD(double S0, double E0, double I0, double R0, double D0, int t0, doub
 
 int main()
 {
+    double S0 = 2798047, E0 = 99, I0 = 0, R0 = 24, D0 = 0;
     FILE* file = fopen("data.txt", "w");
     for (int i = 1; i <= 90; i++) {
-        fprintf(file, "%d  %f\n", i, SEIRD(2798047, 99, 0, 24, 0, 0, i, 0.01));
+        fprintf(file, "%d  %f\n", i, SEIRD(S0, E0, I0, R0, D0, 0, i, 1));
     }
     fclose(file);
 }
