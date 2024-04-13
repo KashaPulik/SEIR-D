d21f76c952490e367cf109d594ef9f72e4a8f00a
Kulik Pavel <pashakulik58@gmail.com>
2024-04-13 19:31:08 +0700

    Third try doesn't work

--- main.c
+++ main.c
@@ -8,17 +8,13 @@ double N(double S, double E, double I, double R, double D)
 double SEIRD(double S0, double E0, double I0, double R0, double D0, int t0, int t, double h)
 {
     double S = S0, E = E0, I = I0, R = R0, D = D0;
-    double Sn, En, In;
     int i = t0;
     for (; i < t - 1; i++) {
-        Sn = S0 + h * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0));
-        En = E0 + h * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0) - 0.994 * E0);
-        In = I0 + h * (0.042 * E0 - 1.0178 * I0);
-        S = S0 + h * 0.5 * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0) + ((0.999 * Sn) / N(S0, E0, I0, R0, D0)) * (In + En));
-        E = E0 + h * 0.5 * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0) - 0.994 * E0 + ((0.999 * Sn) / N(S0, E0, I0, R0, D0)) * (In + En) - 0.994 * En);
-        I = I0 + h * 0.5 * (0.042 * E0 - 1.0178 * I0 + 0.042 * En - 1.0178 * In);
-        R = R0 + h * 0.5 * (0.999 * I0 + 0.952 * E0 + 0.999 * I + 0.952 * E);
-        D = D0 + h * 0.5 * (0.0188 * I0 + 0.0188 * I);
+        S = S0 + h * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0));
+        E = E0 + h * (((0.999 * S0) / N(S0, E0, I0, R0, D0)) * (I0 + E0) - 0.994 * E0);
+        I = I0 + h * (0.042 * E0 - 1.0178 * I0);
+        R = R0 + h * (0.999 * I0 + 0.952 * E0);
+        D = D0 + h * (0.0188 * I0);
         S0 = S;
         E0 = E;
         I0 = I;
